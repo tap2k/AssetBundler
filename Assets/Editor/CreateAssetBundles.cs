561dd27614d7ce2b9fbcbe92935fb930f42262af
tap2k <tparikh@gmail.com>
2023-01-31 09:56:04 -0500

    make things a bit easier and faster

--- Assets/Editor/CreateAssetBundles.cs
+++ Assets/Editor/CreateAssetBundles.cs
@@ -12,11 +12,11 @@ public class CreateAssetBundles
     private static string _password = "abcdef1234";
     private static string _channelID = "tsp53";
     private static string _baseURL = "https://remaking.represent.org/strapi";
-    
-    private static string[] _assetBundleDirectories = { Application.streamingAssetsPath + "/Android", Application.streamingAssetsPath + "/PC", Application.streamingAssetsPath + "/WebGL", Application.streamingAssetsPath + "/Mac"};
-    private static BuildTarget[] _targetPlatforms = { BuildTarget.Android, BuildTarget.StandaloneWindows, BuildTarget.WebGL, BuildTarget.StandaloneOSX };
-    //private static string[] _assetBundleDirectories = { Application.streamingAssetsPath + "/Android", Application.streamingAssetsPath + "/PC", Application.streamingAssetsPath + "/WebGL"};
-    //private static BuildTarget[] _targetPlatforms = { BuildTarget.Android, BuildTarget.StandaloneWindows, BuildTarget.WebGL};
+
+    private static string[] _assetBundleDirectories = { Application.streamingAssetsPath + "/WebGL" };
+    private static BuildTarget[] _targetPlatforms = { BuildTarget.WebGL };
+    //private static string[] _assetBundleDirectories = { Application.streamingAssetsPath + "/Android", Application.streamingAssetsPath + "/PC", Application.streamingAssetsPath + "/WebGL", Application.streamingAssetsPath + "/Mac"};
+    //private static BuildTarget[] _targetPlatforms = { BuildTarget.Android, BuildTarget.StandaloneWindows, BuildTarget.WebGL, BuildTarget.StandaloneOSX };
     private static List<AssetBundleManifest> _manifests = new List<AssetBundleManifest>();
     private static List<string> _manifestDirs = new List<string>();
 
